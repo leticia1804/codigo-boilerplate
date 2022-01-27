d19d9d2a77451edf3591bfa33d4ea9993d8bc0a3
leticia1804 <89095951+leticia1804@users.noreply.github.com>
2022-01-27 19:40:06 -0300

    Update sketch.js

--- p5.play-boilerplate-master-main/sketch.js
+++ p5.play-boilerplate-master-main/sketch.js
@@ -1,18 +1,27 @@
 const Engine = Matter.Engine; // usado para criar o mecanismo da física
 const World = Matter.World; // usado para criar o mundo físico e adicionar objetos a ele
 const Bodies = Matter.Bodies; // usado para criar os objetos físicos que habitam o mundo.
+const Constraint = Matter.Constraint; // MÓDULO PARA ATIVAR AS FUNÇÕES QUE ENGLOBAM RESTRIÇÃO ENTRE CORPOS
+var engine, world,ground;
 
-var parede1,parede2,parede3;
 
-function setup() {
-  // cria a tela do jogo
-  var canvas = createCanvas(400,400);
 
+function preload() {
  
+}
+function setup() {
+
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
   
+ 
 }
 
 function draw() {
-  background(0); 
-
+  background(189);
+ 
+  Engine.update(engine);
+  
+   
 }
